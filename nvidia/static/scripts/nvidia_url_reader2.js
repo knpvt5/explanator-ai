7bd253bf44abd7e3d96b1706007ffc391786b702
Karan <xkaranx5@gmail.com>
2024-12-18 10:42:14 +0530

    nav link

--- nvidia/static/scripts/nvidia_url_reader2.js
+++ nvidia/static/scripts/nvidia_url_reader2.js
@@ -1,7 +1,15 @@
 const fileInput = document.getElementById('input_file');
 const fileNameDisplay = document.getElementById('file-name');
 
+
 fileInput.addEventListener('change', (event) => {
     const fileName = event.target.files[0]?.name || "No file chosen";
+    if(fileName){
+        fileNameDisplay.style.cssText = `
+        width: 50px;
+        overflow-x: auto;
+        border: 1px solid #ccc;
+    `;
+    }
     fileNameDisplay.textContent = fileName;
 });
