011b04ebf5c0731afb557736c55109dc235c429a
Karan <xkaranx5@gmail.com>
2024-12-25 10:12:05 +0530

    www added

--- explanator_ai/settings.py
+++ explanator_ai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='local_dev_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['explanatorai.site', 'explanator-ai.onrender.com', 'localhost', '127.0.0.1', 'healthcheck.railway.app', 'explanator-ai-production.up.railway.app']
+ALLOWED_HOSTS = ['explanatorai.site', 'www.explanatorai.site', 'explanator-ai.onrender.com', 'localhost', '127.0.0.1', 'healthcheck.railway.app', 'explanator-ai-production.up.railway.app']
 
 
 # Application definition
