7833af201873ac01b5c1a18fbb3005e4f44ecbda
Karan <xkaranx5@gmail.com>
2025-01-02 19:48:07 +0530

    fin n cb done

--- nvidia/static/scripts/nvidia.js
+++ nvidia/static/scripts/nvidia.js
@@ -155,8 +155,9 @@ selectModel.addEventListener('change', (event) => {
 
 // Initialize CodeMirror for Markdown Editor
 const markdownPreviewDiv = document.getElementById("markdown-preview");
-const dataContainer = document.getElementById("markdown-data-container");
-const markdownUrl = dataContainer.getAttribute("data-markdown-url");
+const MDdataContainer = document.getElementById("markdown-data-container");
+const markdownUrl = MDdataContainer.getAttribute("data-markdown-url");
+const viewDocsBtn = document.querySelector(".view-docs-btn");
 
 // Function to fetch and render markdown content
 async function loadMarkdownContent() {
@@ -168,12 +169,19 @@ async function loadMarkdownContent() {
         const markdownContent = await response.text();
 
         // Parse and render the Markdown content
-        markdownPreviewDiv.innerHTML = marked.parse(markdownContent);
+        MDdataContainer.innerHTML = marked.parse(markdownContent);
     } catch (error) {
         console.error("Error loading markdown:", error);
-        markdownPreviewDiv.innerHTML = '<p class="error">Error loading documentation</p>';
+        MDdataContainer.innerHTML = '<p class="error">Error loading documentation</p>';
     }
 }
-
-// on Load render the Markdown content
 loadMarkdownContent();
+
+viewDocsBtn.addEventListener("click", () => {
+    MDdataContainer.style.display = MDdataContainer.style.display === "block" ? "none" : "block";
+    if (MDdataContainer.style.display === "block") {
+        viewDocsBtn.innerHTML = "<i class='fa-solid fa-chevron-up'></i>";
+    } else {
+        viewDocsBtn.innerHTML = "<i class='fa-solid fa-chevron-down'></i>";
+    }
+})
